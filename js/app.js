86762f672479d0b3e57df14e9de1cdf11b703966
c1tizen <timotejferenc@gmail.com>
2021-07-07 00:44:36 +0200

    added browsers folder, new structure for ascii

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 console.log(navigator.userAgent);
-var uaString = navigator.userAgent;
+var uaString = navigator.userAgent.toString();
 
 // OPERATING SYSTEM
 var tempSplit = uaString.split("(");
@@ -9,9 +9,30 @@ console.log(os[0]);
 jsOs.insertAdjacentHTML("beforebegin",os[0]);
 
 // BROWSER
+const browser = ""
+uaString = uaString.toLowerCase();
+console.log(uaString)
+if (uaString.includes("edg")) {
+    jsBr.insertAdjacentHTML("beforebegin","Edge browser");
+    browser = "Edge";
+} else if (uaString.includes("opr")) {
+    jsBr.insertAdjacentHTML("beforebegin","Opera browser");
+    browser = "Opera";
+} else if (uaString.includes("safari") || uaString.includes("opr") === false || uaString.includes("edg") === false) {
+    jsBr.insertAdjacentHTML("beforebegin","Safari browser");
+    browser = "Safari";
+} else if (uaString.includes("trident")) {
+    jsBr.insertAdjacentHTML("beforebegin","Internet Explorer browser");
+    browser = "Internet Explorer";
+} else if (uaString.includes("chrome")) {
+    jsBr.insertAdjacentHTML("beforebegin","Chrome browser");
+    browser = "Chrome";
+} else if (uaString.includes("firefox")) {
+    jsBr.insertAdjacentHTML("beforebegin","Firefox browser");
+    browser = "Firefox";
+}
+console.log(browser+"<----")
 var tempBr = tempSplit[1].split(" ");
-var br = tempBr[tempBr.length - 1].replace("/"," ") +" - "+ tempSplit[0].replace("/"," ");
-jsBr.insertAdjacentHTML("beforebegin",br);
 
 
 // SHORT OS
@@ -21,3 +42,7 @@ console.log(tempBr);
 
 // SCREEN SIZE
 window.screen.height;
+
+// ASCII PORT
+console.log(br)
+//asciiPort.attr("src",window.location.hostname+"/"+browser)
