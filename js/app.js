d80830597ca64c01588cbb735ad0f99406cd42dd
c1tizen <timotejferenc@gmail.com>
2021-07-07 10:31:17 +0200

    js syntax fixes, screen w and h added

--- js/app.js
+++ js/app.js
@@ -6,15 +6,20 @@ var tempSplit = uaString.split("(");
 console.log(tempSplit);
 var os = tempSplit[1].split(";");
 console.log(os[0]);
+var jsOs = document.getElementById("jsOs");
 jsOs.insertAdjacentHTML("beforebegin",os[0]);
 
 // BROWSER
-var browser = ""
+var browser = "";
+var jsBr = document.getElementById("jsBr");
 uaString = uaString.toLowerCase();
-console.log(uaString)
+console.log(uaString);
 if (uaString.indexOf("trident") !== -1) {
     jsBr.insertAdjacentHTML("beforebegin","Internet Explorer");
     browser = "Internet Explorer";
+} else if (uaString.includes("navigator")) {
+    jsBr.insertAdjacentHTML("beforebegin","Netscape Navigator");
+    browser = "Netscape-Navigator";
 } else if (uaString.includes("edg")) {
     jsBr.insertAdjacentHTML("beforebegin","Edge");
     browser = "Edge";
@@ -31,17 +36,19 @@ if (uaString.indexOf("trident") !== -1) {
     jsBr.insertAdjacentHTML("beforebegin","Firefox");
     browser = "Firefox";
 }
-console.log(browser+" <----")
+console.log(browser+" <----");
 var tempBr = tempSplit[1].split(" ");
 
 
 // SHORT OS
-var htmlOs = "<span class='highlight'>" + tempBr[tempBr.length - 5] + "</span>"
+var htmlOs = "<span class='highlight'>" + browser.toLowerCase() + "</span>";
+var shortOs = document.getElementById("shortOs");
 shortOs.insertAdjacentHTML("beforebegin", htmlOs);
 console.log(tempBr);
 
 // SCREEN SIZE
-window.screen.height;
+var whOut = document.getElementById("resolution");
+whOut.insertAdjacentHTML("beforebegin", window.screen.height + " x " + window.screen.width);
 
 // ASCII PORT
 //asciiPort.attr("src",window.location.hostname+"/"+browser)
