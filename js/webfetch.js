07eee4a83c109285a7bf2a7f349e9f3ea67134b5
c1tizen <timotejferenc@gmail.com>
2021-09-21 14:01:52 +0200

    .trunc()-ate screen dimensions

--- js/webfetch.js
+++ js/webfetch.js
@@ -83,7 +83,9 @@ function isRetina(){
     return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio >= 2)) && /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
 }
 */
-whOut.insertAdjacentHTML("beforebegin", window.screen.width * window.devicePixelRatio + " x " + window.screen.height * window.devicePixelRatio);
+var widthNoFloat = Math.trunc(window.screen.width * window.devicePixelRatio);
+var heightNoFloat = Math.trunc(window.screen.height * window.devicePixelRatio);
+whOut.insertAdjacentHTML("beforebegin", widthNoFloat + " x " + heightNoFloat);
 /*
 if (isRetina()) {
     whOut.insertAdjacentHTML("beforebegin", window.screen.width*2 + " x " + window.screen.height*2);
